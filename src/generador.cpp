931ac8ff48d426bad9bdf828533bf12972b3d071
NachoOviedo <i.oviedo.1@estudiantec.cr>
2026-03-09 15:17:20 -0600

    Generador terminado con argumentos de inicio desde terminal, -size y -outputpath ademas de incluir escritura eficiente por bloques para los tamaños establecidos SMALL(512mb), MEDIUM(1GB), LARGE(2G).

--- src/generador.cpp
+++ src/generador.cpp
@@ -1,70 +1,95 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <random>
+#include <cstring>
 using namespace std;
-int main()
-{
-    //Obtenemos del usuario el tamaño de archivo a generar
-    cout<<"Ingrese el tamano de el archivo: ";
-    string sizeus;
-    cin>> sizeus;
-    // Depuracion eliminar
-    cout<< sizeus << endl;
 
-    int size;
-    if (sizeus == "peque")
+int main(int argc, char* argv[])
+{
+    //Validacion de informacion correcta
+    if(argc != 5)
     {
-        size = 5;
-    } else if (sizeus == "medi")
+        cout << "Error argumentos invalidos" << endl;
+        return 1;
+    }
+
+    //Obtenemos del usuario el tamaño de archivo a generar
+    string sizeus;
+    // Obtenemos la ruta donde sera creado el archivo
+    string path;
+    for (int i = 1; i < argc; i++)
     {
-        size = 10;
-    } else if (sizeus == "grande")
+        if (strcmp(argv[i], "-size") == 0)
+        {
+            sizeus = argv[i+1];
+        }
+        else if (strcmp(argv[i], "-output") == 0)
+        {
+            path = argv[i+1];
+        }
+    }
+
+    map<string, long long> pesos;
+                    // ll anuncia que es long long
+    pesos ["SMALL"] = 512ll * 1024 * 1024;       //512 Mb
+    pesos ["MEDIUM"] = 1ll * 1024 * 1024 * 1024; // 1 GB
+    pesos ["LARGE"] = 2ll * 1024 * 1024 * 1024;  // 2 Gb
+
+    //Se establece el tamano apartir de la entrada del usuario
+    long long size;
+    auto buscador = pesos.find(sizeus);
+    if (buscador != pesos.end())
     {
-        size = 15;
-    } else
-    {
-        cout<<"Debe escribir una de las siguientes opciones peque, medi o grande"<<endl;
+        size = buscador->second;
+    } else {
+        cout<< "Tamaño ingresado invalido";
+        return 1;
     }
 
     //Creacion o lectura del archivo de los numeros
-    ofstream archivo("C:/Datos 2/PagedArraySorter-Datos-2/src/generados/NumerosRan.txt", ios::binary);
+    ofstream archivo(path, ios::binary);
     if(archivo.fail())
     {
         cout<<"Error al abrir el archivo"<<endl;
         // Salida del programa por error
         exit(1);
     }
+    //Generador de numeros aleatorios
     // Semilla usando random_device
     random_device seed;
-
     // Motor Mersenne Twister inicializado con la semilla
     mt19937 Generador(seed());
-
-    // Distribución: números enteros entre 0 y INT_MAX
+    // Establece que solo se generen numeros enteros entre 0 y INT_MAX
     uniform_int_distribution<int> RandomNum(0, INT_MAX);
 
-    for (int i = 0; i < size; i++)
+    //Puntero del buffer para ecribir por bloques
+    long long BufferSize = size/100/sizeof(int);
+    int* buffer = new int[BufferSize];
+
+
+    //ciclo de escritura por bloques
+    long long BytesPorEscribir = size;
+    long long BytesEscritos = 0;
+
+    while(BytesEscritos < BytesPorEscribir)
     {
-        int num = RandomNum(Generador);
-        cout<<num<<endl;
-        archivo.write((char*)&num, sizeof(num));
+        long long BytesRestantes = BytesPorEscribir - BytesEscritos;
+        //Seleccion de cantidad de enteros por escribir en buffer
+        int enteros = min(BytesRestantes/(long long)sizeof(int), BufferSize);
+
+        //Llenar buffer
+        for (int i = 0; i < enteros; i++){
+            buffer[i] = RandomNum(Generador);
+        }
+        //Escribir Buffer
+        //                  Puntero al primer elemento     Tamano en bytes a escribir
+        archivo.write((char*)buffer,              enteros * sizeof(int));
+        // Actualizar BytesEscritos
+        BytesEscritos += enteros * sizeof(int);
     }
     archivo.close();
-    // Prueba de lectura para asegurarse de funcionamiento correcto Eliminar al terminar depuracion
-    /*
-    ifstream lector("C:/Datos 2/PagedArraySorter-Datos-2/src/generados/NumerosRan.txt", ios::binary);
-    if(lector.fail())
-    {
-        cout << "Error al abrir" << endl;
-        exit(1);
-    }
-    for(int i = 0; i < 5; i++) {
-        int numR;
-        lector.read(reinterpret_cast<char*>(&numR), sizeof(numR));
-        cout << numR << endl;
-    }
-    lector.close();
-    */
-
+    delete[] buffer;
+    cout << "Archivo tamano " << sizeus <<" " << "generado exitosamente" << endl;
     return 0;
 }
