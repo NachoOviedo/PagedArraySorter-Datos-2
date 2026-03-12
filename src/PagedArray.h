a965d0f2c58a6bac64563fcef4b790274f163feb
NachoOviedo <i.oviedo.1@estudiantec.cr>
2026-03-12 13:04:49 -0600

    Implementacion del operador sobrecargado []

--- src/PagedArray.h
+++ src/PagedArray.h
@@ -13,27 +13,36 @@ class PagedArray
 {
     //Variables de la clase
 private:
-    int PageSize;
-    int NumPages;
-    bool* DirtyBite;
-    int** frames;
-    int* FramePages;
-    FILE* file;
-    int PageFaults;
-    int PageHits;
-    int NumElementos;
+    int PageSize;       //Cantidad de enteros por pagina
+    int NumPages;       //Frames disponibles
+    bool* DirtyBite;    //Saber si hay que registrar cambios
+    int** frames;       //Matriz de frames
+    int* FramePages;    //Registro de paginas en frames
+    FILE* file;         //Puntero al archivo en disco
+    int PageFaults;     //Contador de Faultds
+    int PageHits;       //Contador de hits
+    int NumArray;       //Total de enteros en el array
+    int* ultimo;        //Registro de ultimo uso
+    int contador;       //Contador de accesos
+
+    //Metodos declarados
+    void Cargador(int pageNum, int frameIndex);
+    //Metodo para escoger la pagina que se va a guardar en disco
+    int LRU();
 
 public:
     //Constructor
-    PagedArray(const char* filePath, int pageSize, int numPages, int numElementos)
+    PagedArray(const char* filePath, int pageSize, int numPages, int numArray)
     {
+        //asignacion de variables
         PageSize = pageSize;
         NumPages = numPages;
-        NumElementos = numElementos;
+        NumArray = numArray;
         PageFaults = 0;
         PageHits = 0;
         file = fopen(filePath, "r+b");
         frames = new int*[NumPages];
+        contador = 0;
         for(int i = 0; i < NumPages; i++) {
             frames[i] = new int[PageSize];
         }
@@ -47,10 +56,16 @@ public:
         {
             FramePages[i] = -1;
         }
+        ultimo = new int[NumPages];
+        for (int i = 0; i < NumPages; i++)
+        {
+            ultimo[i] = 0;
+        }
     };
     // Destructor
     ~PagedArray()
     {
+        //Limpia la memoria del heap
         delete[] FramePages;
         delete[] DirtyBite;
         for (int i = 0; i < NumPages; i++)
@@ -58,8 +73,24 @@ public:
             delete[] frames[i];
         }
         delete[] frames;
+        delete[] ultimo;
         fclose(file);
     };
+    int& operator[](int index) {
+        int NumPagina = index / PageSize;
+        int PosPagina = index % PageSize;
+
+        for (int i = 0; i < NumPages; i++)
+        {
+            if (FramePages[i] == NumPagina)
+            {
+                PageHits++;
+                return frames[i][PosPagina];
+            }
+        }
+        PageFaults++;
+        return frames[0][PosPagina];
+    }
 };
 
 #endif //PAGEDARRAYSORTER_DATOS_2_PAGEDARRAY_H
